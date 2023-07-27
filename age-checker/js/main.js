31ec102f084e38e9360471f4b3f11808c180b9ee
Frants <118231497+franik7@users.noreply.github.com>
2023-07-27 17:17:56 -0400

    Add files via upload

---
+++ age-checker/js/main.js
@@ -0,0 +1,51 @@
+//Create a conditonal that checks their age
+//If under 16, tell them they can not drive
+//If under 18, tell them they can't hate from outside the club, because they can't even get in
+//If under 21, tell them they can not drink
+//If under 25, tell them they can not rent cars affordably
+//If under 30, tell them they can not rent fancy cars affordably
+//If under over 30, tell them there is nothing left to look forward too
+
+
+
+
+// if(age < 16){
+// console.log("You can't drive")
+// } else if( age < 18) {
+//     console.log("You can't can't hate from outside the club, because you can't even get in")
+// }else if( age < 21) {
+//     console.log("You can't drink")
+// }else if( age < 25) {
+//     console.log("You can't rent cars affordably")
+// }else if( age < 30) {
+//     console.log("You can't rent fancy cars affordably")
+// }else if( age > 30) {
+//     console.log("There is nothing else to look for")
+// }
+
+
+
+
+//--- Harder
+//On click of the h1
+//Take the value from the input
+//Place the result of the conditional in the paragraph
+
+let value = document.querySelector("h1").addEventListener("click", takeVal)
+const p = document.querySelector("p")
+function takeVal(){
+   let age = Number(document.querySelector("input").value)
+   if(age < 16){
+    p.innerText = "You can't drive"
+    } else if( age < 18) {
+        p.innerText = "You can't  hate from outside the club, because you can't even get in"
+    }else if( age < 21) {
+        p.innerText = "You can't drink"
+    }else if( age < 25) {
+        p.innerText = "You can't rent cars affordably"
+    }else if( age < 30) {
+        p.innerText = "You can't rent fancy cars affordably"
+    }else  {
+        p.innerText = "There is nothing else to look for"
+    }
+}
